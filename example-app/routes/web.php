4f71534cf4b7c4850ea612874125e5b2b9ba4919
dorywill <ramdsilvers@gmail.com>
2025-09-12 19:37:52 -0300

    att atvd até a 7

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -4,23 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SeriesController;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//Rota simples
-Route::get('/ola', function(){
-    return "Olá Mundo.";
-});
-
-Route::get('soma', function(){
-    $a = 100;
-    $b = 200;
-    $soma = $a + $b;
-    return "A soma é {$soma}";
-});
-
-// 1. Crie uma rota chamada "/contato" que exiba o texto: "Essa é a página de contato da empresa X".
 Route::get('/contato', function(){
     return "Essa é a página de contato da empresa X";
 });
@@ -43,44 +26,114 @@ route::get('/dataHora', function(){
     return "Data e hora atual: {$dataHora}";
 });
 
+// 5. Crie uma rota chamada "/tecnologias" que exiba em uma lista HTML algumas tecnologias que você gosta (ex.: PHP, Laravel, JavaScript, Python, etc.).
 
-
-//Rota com passagem de parâmetros
-Route::get('/somaa/{v1}/{v2}', function($v1, $v2){
-    $soma = $v1 + $v2;
-    return "A soma é {$soma}";
-});
-
-/**
- * Colocar interrogação no parâmetro (ex. param2),
- * diz que vai ser opcional
- */
-Route::get('/adicionar-produto/{param1}/{param2?}',
-function($p1, $p2 = null) {
-    $texto  = "O parâmetro 1 da URL é {$p1}<br>";
-    if ($p2 != null)
-        $texto .= "O parâmetro 2 da URL é {$p2}<br>";
-    return $texto;
-});
-
-
-Route::get('/series2', function () {
-    $series = [
-                'Lost',
-                'Greys Anatomy',
-                'Agents of SHIELD',
+Route::get('/tecnlogias', function () {
+    $tecnologias = [
+                'PHP',
+                'SQL',
+                'python',
             ];
-    $html = "<ul>";
-    foreach ($series as $serie) {
-        $html .= "<li>$serie</li>";
+    $html = "<h3>Lista HTML </h3><ul>";
+    foreach ($tecnologias as $tecnologia) {
+        $html .= "<li>$tecnologia</li>";
     }
     $html .= "</ul>";
 
     return $html;
 });
 
-Route::get('/series', [SeriesController::class, 'listarSeries']);
+// 6. Crie uma rota chamada "/links" que exiba uma lista com links reais para sites úteis no dia a dia de um dev (ex.:
 
-Route::get('/home', HomeController::class);
-Route::get('/principal', [HomeController::class, 'principal']);
+Route::get('/links', function () {
+    $links = [
+        'Laravel' => 'https://laravel.com/docs',
+        'PHP' => 'https://www.php.net/docs.php',
+        'GitHub' => 'https://github.com',
+        'MySQL' => 'https://dev.mysql.com/doc/',
+    ];
+
+    $html = "<h2>Links sites links</h2><ul>";
+    foreach ($links as $nome => $url) {
+        $html .= "<li><a href=\"$url\" target=\"_blank\">$nome</a></li>";
+    }
+    $html .= "</ul>";
+
+    return $html;
+});
+
+//7. Crie uma rota chamada "/noticias" que exiba uma lista com 3 notícias fictícias de tecnologia.
+
+Route::get('/noticias', function () {
+    $noticias = [
+        'Ataque ao GitHub!!!!! Milhares de diretorios vazados.',
+        'Extra! Prompts que adicionam criptomoedas em sua carteira.',
+        'Java completamente extinto'
+    ];
+
+    $html = "<h3>Notícias </h3><ul>";
+    foreach ($noticias as $noticia) {
+        $html .= "<li>$noticia</li>";
+    }
+    $html .= "</ul>";
+
+    return $html;
+});
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// //Rota simples
+// Route::get('/ola', function(){
+//     return "Olá Mundo.";
+// });
+
+// Route::get('soma', function(){
+//     $a = 100;
+//     $b = 200;
+//     $soma = $a + $b;
+//     return "A soma é {$soma}";
+// });
+
+// //Rota com passagem de parâmetros
+// Route::get('/somaa/{v1}/{v2}', function($v1, $v2){
+//     $soma = $v1 + $v2;
+//     return "A soma é {$soma}";
+// });
+
+// /**
+//  * Colocar interrogação no parâmetro (ex. param2),
+//  * diz que vai ser opcional
+//  */
+// Route::get('/adicionar-produto/{param1}/{param2?}',
+// function($p1, $p2 = null) {
+//     $texto  = "O parâmetro 1 da URL é {$p1}<br>";
+//     if ($p2 != null)
+//         $texto .= "O parâmetro 2 da URL é {$p2}<br>";
+//     return $texto;
+// });
+
+
+// Route::get('/series2', function () {
+//     $series = [
+//                 'Lost',
+//                 'Greys Anatomy',
+//                 'Agents of SHIELD',
+//             ];
+//     $html = "<ul>";
+//     foreach ($series as $serie) {
+//         $html .= "<li>$serie</li>";
+//     }
+//     $html .= "</ul>";
+
+//     return $html;
+// });
+
+// Route::get('/series', [SeriesController::class, 'listarSeries']);
+
+// Route::get('/home', HomeController::class);
+// Route::get('/principal', [HomeController::class, 'principal']);
 
